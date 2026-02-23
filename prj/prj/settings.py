c02d425e81d277241cde436f6b52bdbe5a9f2e39
Dominik Dembinný <dominik.dembinny.s@gyarab.cz>
2026-02-23 15:06:08 +0100

    v1.0.2 - Hot-Fix - AVAVA Database supportment

--- prj/prj/settings.py
+++ prj/prj/settings.py
@@ -75,7 +75,19 @@ WSGI_APPLICATION = 'prj.wsgi.application'
 # Fallback:  local SQLite for development
 
 _database_url = os.environ.get('DATABASE_URL')
+_full_database_setup = os.environ.get('CUSTOM_DATABASE')
 
+if _full_database_setup:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.' + _full_database_setup,
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('PASSWORD'),
+            'HOST': os.getenv('DB_HOST'),
+            'PORT': os.getenv('DB_PORT'),
+        }
+    }
 if _database_url:
     DATABASES = {
         'default': dj_database_url.parse(
